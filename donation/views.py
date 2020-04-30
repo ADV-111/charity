e4956da94498a3faea41d6884de816b02c774889
Mirek Stoklosa <miroslaw.stoklosa@gmail.com>
2020-04-30 20:36:08 +0200

    #2 Strona główna - paginacja instytucji - NIEUDANE

--- donation/views.py
+++ donation/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
@@ -11,12 +12,17 @@ class IndexPage(View):
         quantity = Donation.objects.all().aggregate(Sum('quantity'))
         institution_count = Donation.objects.distinct('institution_id').count()
 
-        foundations = Institution.objects.filter(type__exact='F')
+        foundations_list = Institution.objects.filter(type__exact='F')
         ngos = Institution.objects.filter(type__exact='OP')
         h2h_collections = Institution.objects.filter(type__exact='ZL')
+
+        # paginator_f = Paginator(foundations_list, 4) #TODO: do paginatora ale i tak nie działa
+        # page = request.GET.get('page', 1)
+        # foundations = paginator_f.get_page(page)
+
         return render(request, 'index.html', context={'quantity': quantity,
                                                       'institutions': institution_count,
-                                                      'foundations': foundations,
+                                                      'foundations': foundations_list,
                                                       'ngos': ngos,
                                                       'h2h_collections': h2h_collections})
 
